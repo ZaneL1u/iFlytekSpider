a0257a392e495000b17ed396b0a92878f6daf128
ezreeeeal <zhenze@leju.com>
2022-03-10 00:03:58 +0800

    chore: 增加打包清除dist目录plugin

--- rollup.config.js
+++ rollup.config.js
@@ -2,17 +2,20 @@ import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
 import ts from "rollup-plugin-typescript2";
 import { terser } from "rollup-plugin-terser";
+import cleaner from "rollup-plugin-cleaner";
 
 // 配置
 const rollupConfig = {
   input: "./src/index.ts",
   output: {
-    name: "distinguish",
+    name: "iflytek-spider-api",
     format: "umd",
     dir: "./dist",
   },
-
   plugins: [
+    cleaner({
+      targets: ["./dist"],
+    }),
     resolve([".js", ".ts"]),
     commonjs(),
     ts({
